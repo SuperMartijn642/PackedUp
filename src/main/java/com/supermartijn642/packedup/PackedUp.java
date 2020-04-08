e517df051d5fe53549b3f131db98c48b2c847ac6
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-04-08 17:02:11 +0200

    Initial 1.12

---
+++ src/main/java/com/supermartijn642/packedup/PackedUp.java
@@ -0,0 +1,17 @@
+package com.supermartijn642.packedup;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created 4/8/2020 by SuperMartijn642
+ */
+@Mod(modid = PackedUp.MODID, name = PackedUp.NAME, version = PackedUp.VERSION)
+public class PackedUp {
+
+    public static final String MODID = "packedup";
+    public static final String NAME = "Packed Up";
+    public static final String VERSION = "1.0.5";
+
+
+
+}
