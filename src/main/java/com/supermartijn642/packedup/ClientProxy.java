023e78a212acfddc62e71157e1e55a66896ecdc8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-08-13 20:45:45 +0200

    add Curios support

--- src/main/java/com/supermartijn642/packedup/ClientProxy.java
+++ src/main/java/com/supermartijn642/packedup/ClientProxy.java
@@ -1,17 +1,30 @@
 package com.supermartijn642.packedup;
 
+import com.supermartijn642.packedup.packets.PacketOpenBag;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 /**
  * Created 2/7/2020 by SuperMartijn642
  */
 public class ClientProxy extends CommonProxy {
 
+    private static KeyBinding OPEN_BAG_KEY = new KeyBinding("keys.packedup.openbag", 79/*'o'*/, "keys.category.packedup");
+
     @Override
     public void init(){
         ScreenManager.registerFactory(PackedUp.container, BackpackContainerScreen::new);
+
+        if(ModList.get().isLoaded("curios")){
+            ClientRegistry.registerKeyBinding(OPEN_BAG_KEY);
+            MinecraftForge.EVENT_BUS.addListener(ClientProxy::onKey);
+        }
     }
 
     @Override
@@ -21,5 +34,10 @@ public class ClientProxy extends CommonProxy {
 
     public static void openScreen(String defaultName, String name){
         Minecraft.getInstance().displayGuiScreen(new BackpackRenameScreen(defaultName, name));
+    }
+
+    public static void onKey(InputEvent.KeyInputEvent e){
+        if(OPEN_BAG_KEY.matchesKey(e.getKey(), e.getScanCode()))
+            PackedUp.CHANNEL.sendToServer(new PacketOpenBag());
     }
 }
