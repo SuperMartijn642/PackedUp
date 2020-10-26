c4b3f2f49aeaaef9ce5543f8c2818db3c15733fb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-10-26 14:48:26 +0100

    update version

--- src/main/java/com/supermartijn642/packedup/PackedUp.java
+++ src/main/java/com/supermartijn642/packedup/PackedUp.java
@@ -25,7 +25,7 @@ public class PackedUp {
 
     public static final String MODID = "packedup";
     public static final String NAME = "Packed Up";
-    public static final String VERSION = "1.0.18";
+    public static final String VERSION = "1.0.19";
 
     @Mod.Instance
     public static PackedUp instance;
