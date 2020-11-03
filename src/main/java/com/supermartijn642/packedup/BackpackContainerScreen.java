e959cb152bf8695be7c382b5b4a796949fa5a215
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-11-03 19:49:37 +0100

    initial 1.16.4 port

--- src/main/java/com/supermartijn642/packedup/BackpackContainerScreen.java
+++ src/main/java/com/supermartijn642/packedup/BackpackContainerScreen.java
@@ -25,7 +25,7 @@ public class BackpackContainerScreen extends ContainerScreen<BackpackContainer> 
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
-        this.func_230459_a_(matrixStack, mouseX, mouseY);
+        this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
     }
 
 
