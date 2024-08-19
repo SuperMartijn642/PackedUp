228be5c5ccc00a99dc2d27d9548655ec3eeb08c4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-19 18:30:25 +0200

    Update old backpack data when trying to open backpack

--- src/main/java/com/supermartijn642/packedup/PackedUpCommon.java
+++ src/main/java/com/supermartijn642/packedup/PackedUpCommon.java
@@ -3,9 +3,12 @@ package com.supermartijn642.packedup;
 import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.packedup.extensions.PackedUpPlayer;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +19,9 @@ import java.util.List;
 public class PackedUpCommon {
 
     public static void openBackpackInventory(ItemStack stack, Player player, int bagSlot){
+        // Check the item nbt for a backpack id from before 1.20.5
+        updateOldBackpack(stack);
+
         BackpackType type = ((BackpackItem)stack.getItem()).type;
         Component name = TextComponents.itemStack(stack).get();
         Integer inventoryIndex = stack.get(BackpackItem.INVENTORY_ID);
@@ -47,6 +53,25 @@ public class PackedUpCommon {
         if(backpacks != null){
             backpacks.forEach(newPlayer.getInventory()::placeItemBackInInventory);
             ((PackedUpPlayer)oldPlayer).packedupSetBackpacks(null);
+        }
+    }
+
+    /**
+     * Upgrades the old pre-1.20.5 nbt data to the new data component
+     */
+    private static void updateOldBackpack(ItemStack stack){
+        // Check the item nbt for a backpack id from before 1.20.6
+        if(stack.has(DataComponents.CUSTOM_DATA)){
+            CustomData data = stack.get(DataComponents.CUSTOM_DATA);
+            //noinspection deprecation
+            if(data != null && data.getUnsafe().contains("packedup:invIndex", Tag.TAG_INT)){
+                stack.set(BackpackItem.INVENTORY_ID, data.copyTag().getInt("packedup:invIndex"));
+                //noinspection deprecation
+                if(data.getUnsafe().size() <= 1)
+                    stack.remove(DataComponents.CUSTOM_DATA);
+                else
+                    stack.set(DataComponents.CUSTOM_DATA, data.update(t -> t.remove("packedup:invIndex")));
+            }
         }
     }
 }
