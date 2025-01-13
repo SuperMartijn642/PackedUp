e7352d45cf54e57daad8748de3fde160cbd8e476
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-13 23:49:20 +0100

    Move classes into subpackages

--- src/main/java/com/supermartijn642/packedup/screen/BackpackContainerScreen.java
+++ src/main/java/com/supermartijn642/packedup/screen/BackpackContainerScreen.java
@@ -1,10 +1,11 @@
-package com.supermartijn642.packedup;
+package com.supermartijn642.packedup.screen;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.gui.ScreenUtils;
 import com.supermartijn642.core.gui.widget.BaseContainerWidget;
+import com.supermartijn642.packedup.PackedUpClient;
 import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +15,7 @@ import net.minecraft.resources.ResourceLocation;
  */
 public class BackpackContainerScreen extends BaseContainerWidget<BackpackContainer> {
 
-    private static final ResourceLocation CORNERS = new ResourceLocation("packedup", "textures/corners.png");
+    private static final ResourceLocation CORNERS = new ResourceLocation("packedup", "textures/gui/corners.png");
 
     private Component displayName;
 
--- src/main/java/com/supermartijn642/packedup/storage/BackpackInventory.java
+++ src/main/java/com/supermartijn642/packedup/storage/BackpackInventory.java
@@ -1,6 +1,8 @@
-package com.supermartijn642.packedup;
+package com.supermartijn642.packedup.storage;
 
 import com.google.common.collect.Lists;
+import com.supermartijn642.packedup.BackpackItem;
+import com.supermartijn642.packedup.BackpackType;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
@@ -154,7 +156,9 @@ public class BackpackInventory implements IItemHandlerModifiable {
         compound.putInt("layer", this.layer);
         try{
             NbtIo.write(compound, file);
-        }catch(Exception e){e.printStackTrace();}
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void load(File file){
