ea1dcf78c015bc1e9cb883b4ae3b5d7799bf691d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-13 23:50:32 +0100

    Add customizable backpack icon

--- src/main/java/com/supermartijn642/packedup/PackedUp.java
+++ src/main/java/com/supermartijn642/packedup/PackedUp.java
@@ -11,6 +11,7 @@ import com.supermartijn642.packedup.compat.Compatibility;
 import com.supermartijn642.packedup.generators.*;
 import com.supermartijn642.packedup.packets.PacketOpenBag;
 import com.supermartijn642.packedup.packets.PacketRename;
+import com.supermartijn642.packedup.packets.PacketSetIcon;
 import com.supermartijn642.packedup.screen.BackpackContainer;
 import com.supermartijn642.packedup.storage.BackpackInventory;
 import net.minecraft.core.HolderLookup;
@@ -57,6 +58,7 @@ public class PackedUp {
 
         CHANNEL.registerMessage(PacketRename.class, PacketRename::new, true);
         CHANNEL.registerMessage(PacketOpenBag.class, PacketOpenBag::new, true);
+        CHANNEL.registerMessage(PacketSetIcon.class, PacketSetIcon::new, true);
 
         register();
         if(CommonUtils.getEnvironmentSide().isClient())
@@ -73,9 +75,10 @@ public class PackedUp {
 
         // Backpack items
         for(BackpackType type : BackpackType.values())
-            handler.registerItem(type.getRegistryName(), () -> new BackpackItem(type));
-        // Inventory id data component
+            handler.registerItemCallback(type::registerItem);
+        // Data components
         handler.registerDataComponentType("inventory_id", BackpackItem.INVENTORY_ID);
+        handler.registerDataComponentType("icon", BackpackItem.ICON_ITEM);
         // Container
         handler.registerMenuType("container", () -> BaseContainerType.create(
             (container, data) -> {
