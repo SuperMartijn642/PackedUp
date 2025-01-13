0b24f1dda6fff01df44a3c4e6e5cac251b2b14b0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-13 23:49:20 +0100

    Move classes into subpackages

--- src/main/java/com/supermartijn642/packedup/storage/BackpackStorageManager.java
+++ src/main/java/com/supermartijn642/packedup/storage/BackpackStorageManager.java
@@ -1,7 +1,10 @@
-package com.supermartijn642.packedup;
+package com.supermartijn642.packedup.storage;
 
 import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.util.Holder;
+import com.supermartijn642.packedup.BackpackItem;
+import com.supermartijn642.packedup.BackpackType;
+import com.supermartijn642.packedup.PackedUpConfig;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
