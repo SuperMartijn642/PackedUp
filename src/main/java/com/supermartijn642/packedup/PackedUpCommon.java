cc1f92b51c5056824d01fe2bdf9072584fed792a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-13 23:49:20 +0100

    Move classes into subpackages

--- src/main/java/com/supermartijn642/packedup/PackedUpCommon.java
+++ src/main/java/com/supermartijn642/packedup/PackedUpCommon.java
@@ -2,6 +2,9 @@ package com.supermartijn642.packedup;
 
 import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.TextComponents;
+import com.supermartijn642.packedup.screen.BackpackContainer;
+import com.supermartijn642.packedup.storage.BackpackInventory;
+import com.supermartijn642.packedup.storage.BackpackStorageManager;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
