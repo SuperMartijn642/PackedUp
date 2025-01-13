50517d78b356c475d97c700c0f0e38e4e0b4c387
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-13 23:49:20 +0100

    Move classes into subpackages

--- src/main/java/com/supermartijn642/packedup/screen/DummySlot.java
+++ src/main/java/com/supermartijn642/packedup/screen/DummySlot.java
@@ -1,4 +1,4 @@
-package com.supermartijn642.packedup;
+package com.supermartijn642.packedup.screen;
 
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
--- src/main/java/com/supermartijn642/packedup/storage/BackpackStorageManager.java
+++ src/main/java/com/supermartijn642/packedup/storage/BackpackStorageManager.java
@@ -1,6 +1,9 @@
-package com.supermartijn642.packedup;
+package com.supermartijn642.packedup.storage;
 
 import com.supermartijn642.core.util.Holder;
+import com.supermartijn642.packedup.BackpackItem;
+import com.supermartijn642.packedup.BackpackType;
+import com.supermartijn642.packedup.PackedUpConfig;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
