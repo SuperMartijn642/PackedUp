928d5f56cc30768f136e59349ede86c8c83250ff
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-18 02:22:30 +0100

    Use lambda to only initialize `PackedUpItemInfoGenerator` when running datagen

--- src/main/java/com/supermartijn642/packedup/PackedUp.java
+++ src/main/java/com/supermartijn642/packedup/PackedUp.java
@@ -111,7 +111,8 @@ public class PackedUp implements ModInitializer {
         GeneratorRegistrationHandler handler = GeneratorRegistrationHandler.get("packedup");
         // Register all the generators
         handler.addGenerator(PackedUpAdvancementGenerator::new);
-        handler.addGenerator(PackedUpItemInfoGenerator::new);
+        //noinspection Convert2MethodRef
+        handler.addGenerator(cache -> new PackedUpItemInfoGenerator(cache));
         handler.addGenerator(PackedUpLanguageGenerator::new);
         handler.addGenerator(PackedUpModelGenerator::new);
         handler.addGenerator(PackedUpRecipeGenerator::new);
