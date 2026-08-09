58be9e1d970fb3b68d663caae84dff0fa4811c88
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-09 12:57:42 +0200

    Don't use `IItemHandler` for backpack container slots

---
+++ src/main/java/com/supermartijn642/packedup/screen/BackpackSlot.java
@@ -0,0 +1,119 @@
+package com.supermartijn642.packedup.screen;
+
+import com.supermartijn642.packedup.storage.BackpackInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created 08/03/2023 by SuperMartijn642
+ */
+public class BackpackSlot extends DummySlot {
+
+    private final BackpackInventory inventory;
+
+    public BackpackSlot(BackpackInventory inventory, int index, int x, int y){
+        super(new BackpackInventoryAsContainer(inventory), index, x, y);
+        this.inventory = inventory;
+    }
+
+    @Override
+    public ItemStack getItem(){
+        return this.inventory.getStackInSlot(this.index);
+    }
+
+    @Override
+    public void set(ItemStack stack){
+        this.inventory.setStackInSlot(this.index, stack);
+    }
+
+    @Override
+    public ItemStack remove(int count){
+        return this.inventory.extractItem(this.index, count);
+    }
+
+    @Override
+    public boolean mayPlace(ItemStack stack){
+        return this.inventory.isItemValid(stack);
+    }
+
+    private static class BackpackInventoryAsContainer implements IInventory {
+
+        private final BackpackInventory inventory;
+
+        private BackpackInventoryAsContainer(BackpackInventory inventory){
+            this.inventory = inventory;
+        }
+
+        @Override
+        public void clearContent(){
+            int size = this.inventory.getStacks().size();
+            for(int i = 0; i < size; i++)
+                this.inventory.setStackInSlot(i, ItemStack.EMPTY);
+        }
+
+        @Override
+        public int getContainerSize(){
+            return this.inventory.getStacks().size();
+        }
+
+        @Override
+        public boolean isEmpty(){
+            for(ItemStack stack : this.inventory.getStacks()){
+                if(!stack.isEmpty())
+                    return false;
+            }
+            return true;
+        }
+
+        @Override
+        public ItemStack getItem(int slot){
+            return this.inventory.getStackInSlot(slot);
+        }
+
+        @Override
+        public ItemStack removeItem(int slot, int count){
+            return this.inventory.extractItem(slot, count);
+        }
+
+        @Override
+        public ItemStack removeItemNoUpdate(int slot){
+            ItemStack stack = this.getItem(slot);
+            return this.removeItem(slot, stack.getCount());
+        }
+
+        @Override
+        public void setItem(int slot, ItemStack stack){
+            this.inventory.setStackInSlot(slot, stack);
+        }
+
+        @Override
+        public int getMaxStackSize(){
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public void setChanged(){
+        }
+
+        @Override
+        public boolean stillValid(PlayerEntity player){
+            return true;
+        }
+
+        @Override
+        public void startOpen(PlayerEntity player){
+            throw new IllegalStateException("This should not be called for a slot container!");
+        }
+
+        @Override
+        public void stopOpen(PlayerEntity player){
+            throw new IllegalStateException("This should not be called for a slot container!");
+        }
+
+        @Override
+        public boolean canPlaceItem(int slot, ItemStack stack){
+            return this.inventory.isItemValid(stack);
+        }
+    }
+}
--- src/main/java/com/supermartijn642/packedup/storage/BackpackInventory.java
+++ src/main/java/com/supermartijn642/packedup/storage/BackpackInventory.java
@@ -10,16 +10,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.util.NonNullList;
-import net.minecraftforge.items.IItemHandlerModifiable;
 
-import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.*;
 
 /**
  * Created 2/8/2020 by SuperMartijn642
  */
-public class BackpackInventory implements IItemHandlerModifiable {
+public class BackpackInventory {
 
     private final boolean remote;
     private final ArrayList<ItemStack> stacks = new ArrayList<>();
@@ -57,75 +55,34 @@ public class BackpackInventory implements IItemHandlerModifiable {
         return this.inventoryIndex;
     }
 
-    @Override
-    public int getSlots(){
-        return this.stacks.size();
-    }
-
-    @Nonnull
-    @Override
     public ItemStack getStackInSlot(int slot){
         return this.stacks.get(slot);
     }
 
-    @Nonnull
-    @Override
-    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate){
-        ItemStack current = this.stacks.get(slot);
-        if(!stack.isEmpty() && this.isItemValid(slot, stack) && canStack(current, stack)){
-            int amount = Math.min(stack.getCount(), 64 - current.getCount());
-            if(!simulate){
-                ItemStack newStack = stack.copy();
-                newStack.setCount(current.getCount() + amount);
-                this.stacks.set(slot, newStack);
-
-                if(!this.remote && stack.getItem() instanceof BackpackItem && stack.getOrCreateTag().contains("packedup:invIndex")){
-                    int index = stack.getOrCreateTag().getInt("packedup:invIndex");
-                    if(!this.bagsDirectlyInThisBag.contains(index))
-                        BackpackStorageManager.onInsert(index, this.inventoryIndex);
-                }
-            }
-            ItemStack result = stack.copy();
-            result.shrink(amount);
-            return result;
-        }
-        return stack;
-    }
-
-    @Nonnull
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate){
+    public ItemStack extractItem(int slot, int amount){
         ItemStack stack = this.stacks.get(slot);
         int count = Math.min(amount, stack.getCount());
         ItemStack result = stack.copy();
-        if(!simulate){
-            stack.shrink(count);
+        stack.shrink(count);
 
-            if(!this.remote && result.getItem() instanceof BackpackItem && result.getOrCreateTag().contains("packedup:invIndex")){
-                int index = result.getOrCreateTag().getInt("packedup:invIndex");
-                boolean contains = false;
-                for(ItemStack stack1 : this.stacks){
-                    if(stack1.getItem() instanceof BackpackItem && stack1.getOrCreateTag().contains("packedup:invIndex")
-                        && stack1.getOrCreateTag().getInt("packedup:invIndex") == index){
-                        contains = true;
-                        break;
-                    }
+        if(!this.remote && result.getItem() instanceof BackpackItem && result.getOrCreateTag().contains("packedup:invIndex")){
+            int index = result.getOrCreateTag().getInt("packedup:invIndex");
+            boolean contains = false;
+            for(ItemStack stack1 : this.stacks){
+                if(stack1.getItem() instanceof BackpackItem && stack1.getOrCreateTag().contains("packedup:invIndex")
+                    && stack1.getOrCreateTag().getInt("packedup:invIndex") == index){
+                    contains = true;
+                    break;
                 }
-                if(!contains)
-                    BackpackStorageManager.onExtract(index, this.inventoryIndex);
             }
+            if(!contains)
+                BackpackStorageManager.onExtract(index, this.inventoryIndex);
         }
         result.setCount(count);
         return result;
     }
 
-    @Override
-    public int getSlotLimit(int slot){
-        return 64;
-    }
-
-    @Override
-    public boolean isItemValid(int slot, @Nonnull ItemStack stack){
+    public boolean isItemValid(ItemStack stack){
         if(stack.getItem() instanceof BackpackItem && !this.isBagAllowed(stack))
             return false;
 
@@ -180,8 +137,7 @@ public class BackpackInventory implements IItemHandlerModifiable {
         this.layer = compound.getInt("layer");
     }
 
-    @Override
-    public void setStackInSlot(int slot, @Nonnull ItemStack stack){
+    public void setStackInSlot(int slot, ItemStack stack){
         ItemStack oldStack = this.stacks.get(slot);
         this.stacks.set(slot, ItemStack.EMPTY);
 
