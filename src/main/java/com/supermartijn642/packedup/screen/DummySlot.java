6a3e50a4a2636841db3d86cdb14e5b5f9d6a22bb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-09 12:57:42 +0200

    Don't use `IItemHandler` for backpack container slots

---
+++ src/main/java/com/supermartijn642/packedup/screen/DummySlot.java
@@ -0,0 +1,42 @@
+package com.supermartijn642.packedup.screen;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * Created 29/01/2023 by SuperMartijn642
+ */
+public class DummySlot extends Slot {
+
+    public DummySlot(Container container, int index, int x, int y){
+        super(container, index, x, y);
+    }
+
+    @Override
+    public ItemStack getItem(){
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public void set(ItemStack itemStack){
+    }
+
+    @Override
+    public void setChanged(){
+    }
+
+    @Override
+    public int getMaxStackSize(){
+        ItemStack stack = this.getItem();
+        return stack.isEmpty() ? 64 : stack.getMaxStackSize();
+    }
+
+    @Override
+    public ItemStack remove(int count){
+        ItemStack stack = this.getItem();
+        ItemStack result = stack.split(count);
+        this.set(stack);
+        return result;
+    }
+}
